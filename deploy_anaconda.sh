dabb75dcdc7f4185ac9341021bfb8aa95e6de7a7
langep <patrick.l.lange@gmail.com>
2019-06-04 21:00:50 -0700

    Another test

--- deploy_anaconda.sh
+++ deploy_anaconda.sh
@@ -5,9 +5,6 @@
 # >>> anaconda auth -c -n travis --max-age 307584000 --url https://anaconda.org/USERNAME/PACKAGENAME --scopes "api:write api:read"
 set -e
 
-echo "Sourcing conda.sh"
-source "$HOME"/miniconda/etc/profile.d/conda.sh
-
 echo "Deploying to Anaconda.org..."
 anaconda -t $ANACONDA_TOKEN upload conda-bld/**/crc32c-*.tar.bz2
 
